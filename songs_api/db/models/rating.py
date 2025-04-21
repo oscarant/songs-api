f016137eb4d60d546ab6dd1839089fba6fd533b5
Oscar Pacheco <oscarandrespacheco@gmail.com>
2025-04-21 16:25:55 +0300

    add rating db model

---
+++ songs_api/db/models/rating.py
@@ -0,0 +1,19 @@
+from typing import ClassVar
+
+from beanie import Document, Indexed
+from bson import ObjectId
+from pydantic import Field
+
+
+class Rating(Document):
+    """Beanie Document for a song rating."""
+
+    song_id: ObjectId = Indexed()  # Reference to the Song document
+    rating: int = Field(ge=1, le=5)  # Rating between 1 and 5 inclusive
+
+    class Settings:
+        name: ClassVar[str] = "ratings"
+        indexes: ClassVar[list] = [  # type: ignore[type-arg]
+            [("song_id", 1)],  # look up ratings by song
+            [("song_id", 1), ("rating", 1)],  # optimize aggregated queries
+        ]
